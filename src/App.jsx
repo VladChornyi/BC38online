26da12234aee38b762f0aece91224e505e953632
Vlad <4ornii.vlad@gmail.com>
2022-12-20 09:17:41 +0200

    add start files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { UsersList } from 'components';
+
+export const App = () => {
+  return (
+    <>
+      <a href="">my LInk</a>
+      {/* {<UsersList />} */}
+    </>
+  );
+};
---
+++ src/components/Users/UsersItem.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+
+import styles from './UsersItem.module.css';
+
+console.log(styles);
+
+const getColor = () => {
+  return Math.random() < 0.5 ? 'red' : 'green';
+};
+
+export const UsersItem = ({ user }) => {
+  const { name, email, bio, skills, isOpenToWork } = user;
+
+  return (
+    <>
+      <p>Name: {name}</p>
+      <p>
+        Email:{' '}
+        <a className={styles.link + ' ' + styles.colorRed} href={email}>
+          {email}
+        </a>
+      </p>
+
+      <p style={{ backgroundColor: getColor() /* -> 'red' | 'green' */ }}>BIO: {bio}</p>
+
+      <p>Skills: {skills}</p>
+
+      <p>{isOpenToWork ? 'Open to work' : 'Not open to work'}</p>
+    </>
+  );
+};
+
+// UsersItem.propTypes = {
+//   data: PropTypes.exact({
+//     id: PropTypes.number.isRequired,
+//     name: PropTypes.string.isRequired,
+//     email: PropTypes.string.isRequired,
+//     bio: PropTypes.string.isRequired,
+//     skills: PropTypes.arrayOf(PropTypes.string).isRequired,
+//     isOpenToWork: PropTypes.bool.isRequired,
+//   }),
+// };
