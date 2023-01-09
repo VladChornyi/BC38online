c5b73444b5ba487d949d29cfb11493f81f933784
Vlad <4ornii.vlad@gmail.com>
2023-01-09 09:23:18 +0200

    a/q lessons

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -5,14 +5,21 @@ import axios from 'axios';
 const postsService = axios.create({
   baseURL: 'http://70.34.201.18:8080/posts',
   params: {
-    limit: 4,
+    limit: 6,
   },
 });
 
 export const getPosts = async page => {
   const { data } = await postsService.get('', { params: { page } });
   return data;
+  // return postsService.get('', { params: { page } }).then(data => data.data);
 };
+
+// export const someFunc = () => {
+//   const data = postsService.get('', { params: { page } });
+//   console.log('data', data);
+//   return data;
+// };
 
 // export const getPostById = async (id = 112) => {
 //   const { data } = await postsService.get(`/${id}`);
